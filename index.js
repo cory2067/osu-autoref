c35f2b2e500126d59c487b92d09e8d944dab4899
Cory Lynch <cjl2625@gmail.com>
2019-01-20 23:49:41 -0500

    Add map change/invite commands.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const api = new nodesu.Client(config.apiKey);
 let channel, lobby;
 
 // populate mappool with map info
-function initPool(pool) {
+function initPool() {
   return Promise.all(pool.map(async (b) => {
     const info = (await api.beatmaps.getByBeatmapId(b.id))[0];
     b.name = b.code + ': ' + info.artist + ' - ' + info.title + ' [' + info.version + ']';
@@ -25,7 +25,7 @@ function initPool(pool) {
 // Creates a new multi lobby
 async function init() {
   console.log(chalk.bold.cyan('Starting osu!autoref'));
-  await initPool(pool);
+  await initPool();
   console.log(chalk.bold.green('Loaded map pool!'));
   console.log(chalk.cyan('Attempting to connect...'));
   
@@ -48,10 +48,49 @@ async function init() {
   console.log(chalk.bold.green("Lobby created!"));
   console.log(chalk.bold.cyan(`Name: ${lobby.name}, password: ${password}`));
   console.log(chalk.bold.cyan(`Multiplayer link: https://osu.ppy.sh/mp/${lobby.id}`));
+  console.log(chalk.cyan(`Open in your irc client with "/join #mp_${lobby.id}"`));
 
   lobby.setSettings(bancho.BanchoLobbyTeamModes.TeamVs, bancho.BanchoLobbyWinConditions.ScoreV2);
 
   createListeners();
+}
+
+// Sets current beatmap by matching a user input
+function setBeatmap(input, force=false) {
+  let isCode = !isNaN(input.slice(-1)); //is a numbered map code like NM2, DT1, etc.
+  if (force || input.length > 4 || (msg.message.length > 2 && isCode)) {
+    
+    const codeResult = pool.filter((map) => {
+      return map.code.toLowerCase() === input.toLowerCase();
+    });
+
+    const result = pool.filter((map) => {
+      return map.name.toLowerCase().includes(input.toLowerCase());
+    });
+
+    // Prioritize matches to map code before checking by name
+    let map;
+    if (codeResult.length === 1) {
+      map = codeResult[0];
+    }  else if(result.length === 1) {
+      map = result[0];
+    } else {
+      return;
+    }
+  
+    // Find correct mods based on map code
+    let mapType = map.code.slice(0, 2);
+    let mod = 'Freemod';
+    if (['HD', 'HR', 'DT'].includes(mapType)) {
+      mod = mapType;
+    } else if (mapType === 'NM') {
+      mod = 'None';
+    }
+  
+    channel.sendMessage("Selecting " + map.name);
+    lobby.setMap(map.id);
+    lobby.setMods(mod, false);
+  }
 }
 
 // Respond to events occurring in lobby
@@ -78,8 +117,34 @@ function createListeners() {
     lobby.startMatch(10);
   });
 
-  channel.on("message", (msg) => {
-    console.log(chalk.dim(msg));
+  channel.on("message", async (msg) => {
+    // All ">" commands must be sent by host
+    console.log(chalk.dim(`${msg.user.ircUsername}: ${msg.message}`));
+    if (msg.message.startsWith(">") && msg.user.ircUsername === config.username) {
+      const m = msg.message.substring(1).split(' ');
+      console.log(chalk.yellow(`Received command "${m[0]}"`));
+
+      switch (m[0]) {
+        case 'close':
+          await close();
+          break;
+        case 'invite':
+          const players = match.teams[0].members.concat(match.teams[1].members);
+          for (const p of players) {
+            // intentionally fire these synchronously
+            await lobby.invitePlayer(p);
+          }
+          break;
+        case 'set':
+          setBeatmap(m.slice(1).join(' '), true);
+          break;
+        case 'ping':
+          channel.sendMessage("pong");
+          break;
+        default:
+          console.log(chalk.bold.red(`Unrecognized command "${m[0]}"`));
+      }
+    }
   });
 }
 
@@ -87,13 +152,10 @@ async function close() {
   console.log(chalk.cyan("Closing..."));
   await lobby.closeLobby();
   await client.disconnect();
+  console.log(chalk.cyan("Closed."));
 }
 
 init()
   .then(() => {
-
-    return close();
+    console.log(chalk.dim("Listening..."));
   })
-  .then(() => {
-    console.log(chalk.bold.green("Closed osu!autoref"));
-  });
