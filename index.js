2e72d5d79675e2fd3716e2c5a0d44370a2a0ecb1
Cory Lynch <cjl2625@gmail.com>
2019-01-20 21:48:05 -0500

    Fix tabs.

--- index.js
+++ index.js
@@ -16,84 +16,84 @@ let channel, lobby;
 // populate mappool with map info
 function initPool(pool) {
   return Promise.all(pool.map(async (b) => {
-		const info = (await api.beatmaps.getByBeatmapId(b.id))[0];
-		b.name = b.code + ': ' + info.artist + ' - ' + info.title + ' [' + info.version + ']';
-		console.log(chalk.dim(`Loaded ${info.title}`));
+    const info = (await api.beatmaps.getByBeatmapId(b.id))[0];
+    b.name = b.code + ': ' + info.artist + ' - ' + info.title + ' [' + info.version + ']';
+    console.log(chalk.dim(`Loaded ${info.title}`));
   }));
 }
 
 // Creates a new multi lobby
 async function init() {
-	console.log(chalk.bold.cyan('Starting osu!autoref'));
-	await initPool(pool);
-	console.log(chalk.bold.green('Loaded map pool!'));
-	console.log(chalk.cyan('Attempting to connect...'));
-	
-	try {
-		await client.connect();
-		console.log(chalk.bold.green("Connected to Bancho!"));
-		channel = await client.createLobby(`${match.tournament}: ${match.teams[0].name} vs ${match.teams[1].name}`);
-	} catch (err) {
-		console.log(err);
-		console.log(chalk.bold.red("Failed to create lobby"));
-		process.exit(1);
-	}
+  console.log(chalk.bold.cyan('Starting osu!autoref'));
+  await initPool(pool);
+  console.log(chalk.bold.green('Loaded map pool!'));
+  console.log(chalk.cyan('Attempting to connect...'));
+  
+  try {
+    await client.connect();
+    console.log(chalk.bold.green("Connected to Bancho!"));
+    channel = await client.createLobby(`${match.tournament}: ${match.teams[0].name} vs ${match.teams[1].name}`);
+  } catch (err) {
+    console.log(err);
+    console.log(chalk.bold.red("Failed to create lobby"));
+    process.exit(1);
+  }
 
-	lobby = channel.lobby;
+  lobby = channel.lobby;
 
-	const password = Math.random().toString(36).substring(8);
-	await lobby.setPassword(password);
- 	await lobby.setMap(1262832); //hitorigoto dayo
+  const password = Math.random().toString(36).substring(8);
+  await lobby.setPassword(password);
+   await lobby.setMap(1262832); //hitorigoto dayo
 
-	console.log(chalk.bold.green("Lobby created!"));
-	console.log(chalk.bold.cyan(`Name: ${lobby.name}, password: ${password}`));
-	console.log(chalk.bold.cyan(`Multiplayer link: https://osu.ppy.sh/mp/${lobby.id}`));
+  console.log(chalk.bold.green("Lobby created!"));
+  console.log(chalk.bold.cyan(`Name: ${lobby.name}, password: ${password}`));
+  console.log(chalk.bold.cyan(`Multiplayer link: https://osu.ppy.sh/mp/${lobby.id}`));
 
-	lobby.setSettings(bancho.BanchoLobbyTeamModes.TeamVs, bancho.BanchoLobbyWinConditions.ScoreV2);
+  lobby.setSettings(bancho.BanchoLobbyTeamModes.TeamVs, bancho.BanchoLobbyWinConditions.ScoreV2);
 
-	createListeners();
+  createListeners();
 }
 
 // Respond to events occurring in lobby
 function createListeners() {
-	lobby.on("playerJoined", (obj) => {
-		const name = obj.player.user.username;
-		console.log(chalk.yellow(`Player ${name} has joined!`));
+  lobby.on("playerJoined", (obj) => {
+    const name = obj.player.user.username;
+    console.log(chalk.yellow(`Player ${name} has joined!`));
 
-		// Attempt to auto-assign team
-		if (match.teams[0].members.includes(name)) {
-		  lobby.changeTeam(obj.player, "Blue");
+    // Attempt to auto-assign team
+    if (match.teams[0].members.includes(name)) {
+      lobby.changeTeam(obj.player, "Blue");
     } else if (match.teams[1].members.include(name)) {
-		  lobby.changeTeam(obj.player, "Red");
+      lobby.changeTeam(obj.player, "Red");
     } else {
-			console.log(chalk.red("Warning! Couldn't figure out team"));
-		}
+      console.log(chalk.red("Warning! Couldn't figure out team"));
+    }
 
     if (obj.player.user.isClient()) {
-			lobby.setHost("#" + obj.player.user.id);
-		}
-	 });
+      lobby.setHost("#" + obj.player.user.id);
+    }
+   });
 
-	lobby.on("allPlayersReady", (obj) => {
-		lobby.startMatch(10);
-	});
+  lobby.on("allPlayersReady", (obj) => {
+    lobby.startMatch(10);
+  });
 
-	channel.on("message", (msg) => {
-		console.log(chalk.dim(msg));
-	});
+  channel.on("message", (msg) => {
+    console.log(chalk.dim(msg));
+  });
 }
 
 async function close() {
-	console.log(chalk.cyan("Closing..."));
-	await lobby.closeLobby();
-	await client.disconnect();
+  console.log(chalk.cyan("Closing..."));
+  await lobby.closeLobby();
+  await client.disconnect();
 }
 
 init()
   .then(() => {
 
-		return close();
+    return close();
   })
   .then(() => {
-		console.log(chalk.bold.green("Closed osu!autoref"));
+    console.log(chalk.bold.green("Closed osu!autoref"));
   });
